13457dc02935834a6550349b3ead52e07d1d5916
Matthew Yates <Mat.C.Yates@gmail.com>
2020-10-02 16:45:10 -0400

    added basic movement, might replace with rigid body based controller later

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-  public float mouseSensitivity = 100f;
+  [SerializeField] float mouseSensitivity = 110f;
   [SerializeField] Transform playerBody;
 
   float xRotation = 0f;
+  float mouseX = 0f;
+  float mouseY = 0f;
+
 
   // Start is called before the first frame update
   void Start()
@@ -18,14 +21,17 @@ public class MouseLook : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-    float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+    mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+    mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
     xRotation -= mouseY;
-    xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    xRotation = Mathf.Clamp(xRotation, -89f, 89f);
+
 
     transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
     playerBody.Rotate(Vector3.up * mouseX);
+
 
 
 
