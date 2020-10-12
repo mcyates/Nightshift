234bc1d69abac3f2263a9f50a3e4e8c46eb13220
Matthew Yates <Mat.C.Yates@gmail.com>
2020-10-12 14:44:50 -0400

    basic rigidbody movement

--- Assets/Scripts/RigidBodyFPS.cs
+++ Assets/Scripts/RigidBodyFPS.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,6 +9,10 @@ public class RigidBodyFPS : MonoBehaviour
 {
 
   [SerializeField] InputMaster controls;
+  float moveForward;
+  float moveSide;
+  Vector2 movement = new Vector2();
+  Rigidbody player;
 
   private void OnEnable()
   {
@@ -25,12 +30,31 @@ public class RigidBodyFPS : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
+    player = GetComponent<Rigidbody>();
 
   }
 
   // Update is called once per frame
   void Update()
   {
+    ProcessInput();
+    ProcessMovement();
+  }
 
+  private void ProcessInput()
+  {
+    controls.Player.MovementHorizontal.performed += context => movement.x = context.ReadValue<float>();
+    controls.Player.MovementVertical.performed += context => movement.y = context.ReadValue<float>();
+
+
+  }
+
+
+  private void ProcessMovement()
+  {
+    // player.AddForce()
+    Vector3 intendedMovement = new Vector3(0, 0, 0);
+    intendedMovement = (transform.forward * movement.x) + (transform.right * movement.y) + (transform.up * player.velocity.y);
+    player.velocity = intendedMovement;
   }
 }
