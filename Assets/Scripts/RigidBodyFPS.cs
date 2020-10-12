ac063dd5212d419fb161b9d66592edaa6040eefd
Matthew Yates <Mat.C.Yates@gmail.com>
2020-10-12 14:14:58 -0400

    added placeholder model

---
+++ Assets/Scripts/RigidBodyFPS.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+public class RigidBodyFPS : MonoBehaviour
+{
+
+  [SerializeField] InputMaster controls;
+
+  private void OnEnable()
+  {
+    if (controls == null)
+    {
+      controls = new InputMaster();
+    }
+    controls.Enable();
+  }
+
+  private void OnDisable()
+  {
+    controls.Disable();
+  }
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
